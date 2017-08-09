7096362992a98141326bd7df6d3d9c3bb6e34c1f
Trung Ho <hmtrung90@gmail.com>
2017-08-09 11:38:27 +0700

    ex3

--- ex3/VolumeDiscount.py
+++ ex3/VolumeDiscount.py
@@ -1,27 +1,26 @@
 import re
 
-class VolumeDiscount():
-
-    def bestDeal(self, priceList, quantity):
-        d = {}
-        for price in priceList:
-            match = re.match("(\w+)\s+(\w+)", price)
-            d[int(match.group(1))] = int(match.group(2))
-        if quantity <  min(d.keys()):
-            result = min(d.values())
+class VolumeDiscount(object):
+    def best_deal(self, price_list, quantity):
+        dictionary = {}
+        for price in price_list:
+            match = re.match(r"(\w+)\s+(\w+)", price)
+            dictionary[int(match.group(1))] = int(match.group(2))
+        if quantity < min(dictionary.keys()):
+            result = min(dictionary.values())
         else:
             unit_price = []
-            for price in d.keys():
-                unit_price.append(d[price] / float(price))
+            for price in dictionary:
+                unit_price.append(dictionary[price] / float(price))
             min_unit_price = min(unit_price)
-            index_min = unit_price.index(min_unit_price)     
-            integer = quantity / d.keys()[index_min]
-            remain = quantity % d.keys()[index_min]            
-            result = integer * d[d.keys()[index_min]]            
-            del d[d.keys()[index_min]]
-            newPriceList = []
-            for key in d.keys():
-                newPriceList.append(str(key) + " "+ str(d[key]))
+            index_min = unit_price.index(min_unit_price)
+            integer = quantity / dictionary.keys()[index_min]
+            remain = quantity % dictionary.keys()[index_min]
+            result = integer * dictionary[dictionary.keys()[index_min]]
+            del dictionary[dictionary.keys()[index_min]]
+            new_price_list = []
+            for key in dictionary:
+                new_price_list.append(str(key) + " " + str(dictionary[key]))
             if remain != 0:
-                result = result + self.bestDeal(newPriceList, remain)              
+                result = result + self.best_deal(new_price_list, remain)
         return result
--- ex3/test.py
+++ ex3/test.py
@@ -1,36 +1,36 @@
 import unittest
-from VolumeDiscount import VolumeDiscount
+from ex3.VolumeDiscount import VolumeDiscount
 
 
 class SimpleTest(unittest.TestCase):
-    
+
     def test1(self):
-        vd = VolumeDiscount()
-        priceList = ["1 10", "5 45"]
+        volume_discount = VolumeDiscount()
+        price_list = ["1 10", "5 45"]
         quantity = 10
         result = 90
-        self.assertEquals(vd.bestDeal(priceList, quantity), result)
-      
+        self.assertEquals(volume_discount.best_deal(price_list, quantity), result)
+
     def test2(self):
-        vd = VolumeDiscount()
-        priceList = ["1 8", "5 45"]
+        volume_discount = VolumeDiscount()
+        price_list = ["1 8", "5 45"]
         quantity = 10
         result = 80
-        self.assertEquals(vd.bestDeal(priceList, quantity), result)
-         
+        self.assertEquals(volume_discount.best_deal(price_list, quantity), result)
+
     def test3(self):
-        vd = VolumeDiscount()
-        priceList = ["99 913", "97 173", "50 464", "80 565"]
+        volume_discount = VolumeDiscount()
+        price_list = ["99 913", "97 173", "50 464", "80 565"]
         quantity = 18
         result = 173
-        self.assertEquals(vd.bestDeal(priceList, quantity), result)
-        
+        self.assertEquals(volume_discount.best_deal(price_list, quantity), result)
+
     def test4(self):
-        vd = VolumeDiscount()
-        priceList = ["2 272","1 166","10 993"]
+        volume_discount = VolumeDiscount()
+        price_list = ["2 272", "1 166", "10 993"]
         quantity = 81
         result = 8110
-        self.assertEquals(vd.bestDeal(priceList, quantity), result) 
-            
+        self.assertEquals(volume_discount.best_deal(price_list, quantity), result)
+
 if __name__ == '__main__':
     unittest.main()
